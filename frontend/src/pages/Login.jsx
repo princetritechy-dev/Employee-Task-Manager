2437bcb3918b52338730af6f2959302d4f85d990
rishavkumar003 <rishavkumar78766@gmail.com>
2026-08-21 13:01:09 +0530

    Updated Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import api from "../api";
-import "../styles/Login.css";
+import "../styles/login.css";
 import { Lock, Mail, Eye, EyeOff, CheckCircle2 } from "lucide-react";
 
 export default function Login() {
